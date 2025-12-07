fb49d0ccd5377884363843d006add5d0b3fcc005
Harish Kanna R G <harishkannarg99@gmail.com>
2025-12-07 17:13:48 +0530

    hello world controller

--- productservice/src/main/java/com/example/productservice/controller/HelloWorldController.java
+++ productservice/src/main/java/com/example/productservice/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.example.productservice.contrller;
+package com.example.productservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
